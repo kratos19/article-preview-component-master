d99b2697626fd1bc2081fa4c188a0396bf221484
Sergiu Stancioiu <just.me019@yahoo.com>
2022-02-22 19:20:24 -0800

    Finished Article Preview Component

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 const shareWrappers = document.querySelectorAll('.share-wrapper');
 const shareState = document.querySelector('.share-state');
 const authorWrapper = document.querySelector('.author-wrapper');
+const shareTooltip = document.querySelector('.share-tooltip');
 
 shareWrappers.forEach(function (share) {
     share.addEventListener('click', function (event) {
-        shareState.classList.toggle('active');
-        authorWrapper.classList.toggle('hidden');
+        if (window.screen.width < 731) {
+            shareState.classList.toggle('active');
+            authorWrapper.classList.toggle('hidden');
+        } else {
+            shareTooltip.classList.toggle('active');
+        }
     });
 });
